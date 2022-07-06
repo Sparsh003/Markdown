fefbbcf65469236a4499384e9c21ec79b3a81c32
Sparsh003 <sparsh.dev3@gmail.com>
2022-07-06 18:05:12 +0530

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/Nav";
+import Dashboard from "./components/Dashboard";
+import translate from "./images/translate.png";
+import book from "./images/open-book.png";
+import list from "./images/list.png";
+import git from "./images/git.png";
+import question from "./images/question.png";
+import flag from "./images/red-flag.png";
+import link from "./images/link.png";
+import view from "./images/view.png";
+import alert from "./images/alert.png";
+import comment from "./images/comment.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Dashboard />
+
+      <div className="stickyIcons">
+        <ul>
+          <li>
+            <img className="m-4 p-1" src={translate} alt="translate" />
+          </li>
+          <li>
+            <img className="m-4 p-1" src={book} alt="book" />
+          </li>
+
+          <li>
+            <img className="m-4 p-1" src={link} alt="link" />
+          </li>
+          <li>
+            <img className="m-4 p-1" src={comment} alt="comment" />
+          </li>
+          <li>
+            <img className="m-4 p-1" src={list} alt="list" />
+          </li>
+          <li>
+            <img className="m-4 p-1" src={alert} alt="alert" />
+          </li>
+          <li>
+            <img className="m-4 p-1" src={view} alt="view" />
+          </li>
+          <li>
+            <img className="m-4 p-1" src={question} alt="question" />
+          </li>
+          <li>
+            <img className="m-4 p-1" src={git} alt="git" />
+          </li>
+          <li>
+            <img className="m-4 p-1 " src={flag} alt="flag" />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
